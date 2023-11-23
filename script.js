cadf0a9fb4197534f1d4cd79164d26d6ab56024b
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-23 13:17:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstChar(text) {
   // your code here
+	 if(text.length === 0){
+
+        return "";
+    }
+    text = text.replace(" ","");
+    return text[0];
 }
 
 // Do not change the code below
